f0586c8a3c0b9d68a612f7fa36cf378989f51aa9
WindAsMe <542636539@qq.com>
2018-07-30 20:20:38 +0800

    NUS Smart Home Python

--- software/resource/dataSaveResource.py
+++ software/resource/dataSaveResource.py
@@ -6,7 +6,7 @@
 # Location:/Home/PycharmProjects/..
 import web
 import datetime
-db = web.database(dbn='mysql', db='SmartHome', user='root', pw='change123')
+db = web.database(dbn='mysql', db='SmartHome', user='root', host='13.76.133.103', pw='change123')
 
 
 def insert_humid(humid):
@@ -29,3 +29,8 @@ def insert_temp(temp):
         except IndexError:
             return None
 
+
+if __name__ == '__main__':
+    insert_humid(78.0)
+    insert_pressure(1.0)
+    insert_temp(21.0)
