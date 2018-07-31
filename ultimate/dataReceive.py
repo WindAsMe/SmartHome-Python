d876ee1d5c162cc7e6ae830411e67161558d4512
WindAsMe <542636539@qq.com>
2018-08-01 05:09:09 +0800

    NUS Smart Home Python

--- ultimate/dataReceive.py
+++ ultimate/dataReceive.py
@@ -4,40 +4,32 @@
 # Date     :18-8-1 上午4:26
 # File     :dataReceive.py
 # Location:/Home/PycharmProjects/..
-import MySQLdb
+import urllib2
+
 import open_air
 import close_air
 import open_light
 import close_light
 
-url = 'http://13.67.109.181:8080/man?flag='
-
-
-conn = MySQLdb.connect(host='13.67.109.181',
-                       user='root',
-                       db='SmartHome',
-                       passwd='root',
-                       port=3306,
-                       charset="utf8")
-cursor = conn.cursor()
+url = 'http://13.67.109.181:8080/work'
 
 
 def get_movement():
 
     try:
-        cursor.excute("select id, flag, is_worked from flag order by id DESC")
-        id, flag, is_worked = cursor.fetchall()  # 取出cursor得到的数据
-        print("id, flag, is_worked", id, flag, is_worked)
-        if is_worked:
-            if flag == 1:
+        req = urllib2.Request(url=url)
+        print(req)
+        res = urllib2.urlopen(req)
+        res = res.read()
+        if res["data"]["is_worked"]:
+            if res["data"]["flag"] == 1:
                 open_air.air_open()
-            if flag == 2:
+            if res["data"]["flag"] == 2:
                 close_air.air_close()
-            if flag == 3:
+            if res["data"]["flag"] == 3:
                 open_light.light_open()
-            if flag == 4:
+            if res["data"]["flag"] == 4:
                 close_light.light_close()
-            cursor.execute("UPDATE flag SET is_worked WHERE SEX = '%d'" % id)
     except Exception, e:
         print(e)
 
@@ -46,5 +38,4 @@ if __name__ == '__main__':
     try:
         get_movement()
     except Exception, e:
-        cursor.close()
-        conn.close()  # 最后记得关闭光标和连接，防止数据泄露
+        print(e.message)
